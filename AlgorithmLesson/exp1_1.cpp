3c57b0ced548b47b4a047008e1d09d6fb5e34a18
CheeReus_11 <fanwei1995@hotmail.com>
2019-10-11 13:15:12 +0800

    add experiments

---
+++ AlgorithmLesson/exp1_1.cpp
@@ -0,0 +1,32 @@
+// 整数分划问题
+
+#include <iostream>
+#include <cctype>
+using namespace std;
+int divideInteger (int n, int m);
+
+int main()
+{
+  int n;
+  cout << "Please enter n: " << endl;
+  cin >> n;
+  if (n < 1) {
+    cout << "Error" << endl;
+    return 0;
+  } else {
+    cout << divideInteger(n, n);
+    return 0;
+  }
+}
+
+int divideInteger (int n, int m) {
+  if (n == 1 || m == 1){
+    return 1;
+  } else if (n < m) {
+    return divideInteger(n, n);
+  } else if (n == m) {
+    return 1 + divideInteger(n, n-1);
+  } else {
+    return divideInteger(n, m-1) + divideInteger(n-m, m);
+  }
+}
