1c919627aa469f5b9bcca3d54829692360736c09
CheeReus_11 <fanwei1995@hotmail.com>
2020-04-15 10:27:24 +0800

    add 0415

---
+++ LaoJiu/20200415.cpp
@@ -0,0 +1,21 @@
+/*
+ * @Description: 
+ * @Author: 陈十一
+ * @Date: 2020-04-14 09:38:07
+ * @LastEditTime: 2020-04-15 10:20:41
+ * @LastEditors: 陈十一
+ */
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+  int toes = 10;
+  double toes_2 = toes * 2;
+  double toes_square = pow(toes, 2);
+  cout << "toes：" << toes << endl;
+  cout << "toes的两倍：" << toes_2 << endl;
+  cout << "toes的平方：" << toes_square << endl;
+  return 0;
+}
